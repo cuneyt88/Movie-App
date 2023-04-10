95fbde410c5dd740070934fabfb7c710771292d5
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-04-10 10:01:19 +0300

    structure and register page done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <div>
+      <AppRouter/>
+      <Register />
+    </div>
+  );
+};
+
+export default App;
