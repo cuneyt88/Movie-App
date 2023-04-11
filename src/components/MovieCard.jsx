e436bf46e70ecccbfde0e998a1e33539e8351c25
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-04-11 16:57:31 +0300

    movie detail page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContextProvide';
+import { useNavigate } from 'react-router-dom';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = (poster_path,title,overview,vote_average,id) => {
-  return (
+    const {currentUser}=useContext(AuthContext)
+    const navigate=useNavigate()
+    const getVoteClass=(vote)=>{
+        if(vote>8){
+            return "green"
+        }else if(vote>=6){
+            return "orange"
+        }else{
+            return "red"
+        }
+    }
+  
+    return (
     <div
       className="movie"
-      
+      onClick={()=>navigate("details/"+id)}
     >
       <img
         loading="lazy"
@@ -16,6 +30,7 @@ const MovieCard = (poster_path,title,overview,vote_average,id) => {
         alt="movie-card"
       />
       <div className="flex align-baseline justify-between p-1 text-white">
+        {currentUser && <span className={`tag ${getVoteClass(vote_average)}`}>{vote_average}</span> }
         <h5>{title}</h5>
         
       </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard" 
+
 const API_KEY=process.env.REACT_APP_TMDB_KEY;
   const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
   const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
@@ -8,6 +9,8 @@ const API_KEY=process.env.REACT_APP_TMDB_KEY;
 const Main = () => {
 
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getMovies(FEATURED_API)
@@ -16,12 +19,44 @@ const Main = () => {
   
 
   const getMovies=(API)=>{
-    axios.get(API).then((res)=>console.log(res.data.results)).catch((err)=>console.log(err))
+    setLoading(true)
+    axios.get(API).then((res)=>setMovies(res.data.  
+    results)).catch((err)=>console.log(err)).finally(()=>setLoading(false))
   }
 
-  return <div className="flex justify-center flex-wrap">
-   {movies.map((movie)=> <MovieCard key={movie.id} {...movie}/> )}
-  </div>; 
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    getMovies(SEARCH_API+searchTerm); 
+  }
+
+  return (
+  <>
+  <form className="flex justify-center p-2" onSubmit={handleSubmit}>
+    <input
+      type="search"
+      className="w-80 h-8 rounded-md outline-none border p-1 m-2"
+      placeholder="Search a movie..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+      value={searchTerm}
+    />
+    <button className="dark:text-white" type="submit">
+      Search
+    </button>
+  </form>
+  <div className="flex justify-center flex-wrap">
+    {loading ? (
+      <div
+        className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    ) : (
+      movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+    )}
+  </div>
+</>
+)
 };
 
 export default Main;
